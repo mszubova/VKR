196e977eb3bd93067d2ea9bdb9eaf07bfad30d4d
Maria Zubova <73553427+mszubova@users.noreply.github.com>
2022-05-10 17:24:36 +0500

    create project

---
+++ d_project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="navMenu">
+  <nav-menu></nav-menu>
+</div>
+</template>
+
+<script>
+import navMenu from './components/navMenu.vue'
+export default {
+  component:{
+    'nav-menu': navMenu
+  }
+}
+</script>
+
+<style>
+</style>
